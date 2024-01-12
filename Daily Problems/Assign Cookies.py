0f0854390fe6b0f8ed08f4fe6a2e715f818cc5a2
B M Rauf <bmrauf@outlook.com>
2024-01-13 01:36:57 +0600

    Update Assign Cookies.py

--- Daily Problems/Assign Cookies.py
+++ Daily Problems/Assign Cookies.py
@@ -3,7 +3,8 @@
 
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        g.sort(),s.sort()
+        g.sort()
+        s.sort()
         count, i, j = 0, 0, 0
         while i < len(g) and j < len(s):
             if s[j] >= g[i]:
