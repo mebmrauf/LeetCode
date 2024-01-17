27e69db426c8f12ce0d36a2abd56faad763d0a2d
B M Rauf <115145418+raufbiswas@users.noreply.github.com>
2024-01-17 06:16:44 +0600

    Update Insert Delete GetRandom O(1).py

--- Medium/Insert Delete GetRandom O(1).py
+++ Medium/Insert Delete GetRandom O(1).py
@@ -1,19 +1,24 @@
 # 380. Insert Delete GetRandom O(1)
 # https://leetcode.com/problems/insert-delete-getrandom-o1
 
+import random
+
 class RandomizedSet:
 
     def __init__(self):
-        self.list = []  
+        self.set = set()
+        self.list = []
 
     def insert(self, val: int) -> bool:
-        if val not in self.list:
-            self.list.append(val)
-            return True
-        return False   
+        if val in self.set:
+            return False
+        self.set.add(val)
+        self.list.append(val)
+        return True
 
     def remove(self, val: int) -> bool:
         if val in self.list:
+            self.set.remove(val)
             self.list.remove(val)
             return True
         return False
