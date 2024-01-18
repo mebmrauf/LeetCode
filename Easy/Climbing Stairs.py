2b5f9cf7dfe83a57e50c444c0f42ee4f0bfb66ae
B M Rauf <bmrauf@outlook.com>
2024-01-18 23:54:11 +0600

    Update Climbing Stairs.py

--- Easy/Climbing Stairs.py
+++ Easy/Climbing Stairs.py
@@ -2,6 +2,21 @@
 # https://leetcode.com/problems/climbing-stairs/
 # https://leetcode.com/problems/climbing-stairs/solutions/4584244/solution-for-climbing-stairs-problem/
 
+# MOST EFFICIENT ANSWER
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        previous, current = 1, 1
+        for i in range(2, n+1):
+            temp = current
+            current = previous + current
+            previous = temp
+        return current
+
+# OR
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n <= 2:
