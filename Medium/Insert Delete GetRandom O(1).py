b21fa8e64a0a1e5894a3f78262f014c0ab6d85a3
B M Rauf <bmrauf@outlook.com>
2024-01-29 18:17:01 +0600

    Update Insert Delete GetRandom O(1).py

--- Medium/Insert Delete GetRandom O(1).py
+++ Medium/Insert Delete GetRandom O(1).py
@@ -4,24 +4,26 @@
 import random # No need to import in LeetCode
 
 class RandomizedSet:
-
     def __init__(self):
-        self.set = set()
+        self.dicT = {}
         self.list = []
 
     def insert(self, val: int) -> bool:
-        if val in self.set:
+        if val in self.dicT:
             return False
-        self.set.add(val)
+        self.dicT[val] = len(self.list)
         self.list.append(val)
         return True
 
     def remove(self, val: int) -> bool:
-        if val in self.list:
-            self.set.remove(val)
-            self.list.remove(val)
-            return True
-        return False
+        if val not in self.dicT:
+            return False
+        index, lastElem = self.dicT[val], self.list[-1]
+        self.list[index], self.dicT[lastElem] = lastElem, index
+
+        self.list.pop()
+        del self.dicT[val]
+        return True
 
     def getRandom(self) -> int:
         return random.choice(self.list)
