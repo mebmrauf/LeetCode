57ca3b53b4edb7fe1107579b1fcd426022c14892
B M Rauf <bmrauf@outlook.com>
2024-02-16 01:09:56 +0600

    Update

--- Medium/Rearrange Array Elements by Sign.py
+++ Medium/Rearrange Array Elements by Sign.py
@@ -1,6 +1,8 @@
 # 2149. Rearrange Array Elements by Sign
 # https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
 
+from typing import List
+
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         l = len(nums)
