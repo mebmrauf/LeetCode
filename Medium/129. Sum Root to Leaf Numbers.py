958ca54dff9c7bf2b4d3fe15c044820fb2607897
B M Rauf <bmrauf@outlook.com>
2024-04-15 19:36:30 +0600

    Create

---
+++ Medium/129. Sum Root to Leaf Numbers.py
@@ -0,0 +1,22 @@
+# 129. Sum Root to Leaf Numbers
+# https://leetcode.com/problems/sum-root-to-leaf-numbers/
+
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, currentSum):
+            if not node:
+                return 0
+            currentSum = currentSum * 10 + node.val
+            if not node.left and not node.right:
+                return currentSum
+            return dfs(node.left, currentSum) + dfs(node.right, currentSum)
+        
+        return dfs(root, 0)
