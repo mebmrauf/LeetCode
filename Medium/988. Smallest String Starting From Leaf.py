1903e1b8549c1cb9e28ea6b8b364ddaeafe10927
B M Rauf <bmrauf@outlook.com>
2024-04-18 02:26:21 +0600

    Create

---
+++ Medium/988. Smallest String Starting From Leaf.py
@@ -0,0 +1,25 @@
+# 988. Smallest String Starting From Leaf
+# https://leetcode.com/problems/smallest-string-starting-from-leaf/
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def smallestFromLeaf(self, root: TreeNode) -> str:
+        def dfs(node, path):
+            if not node:
+                return
+            path.append(chr(node.val + ord('a')))
+            if not node.left and not node.right:
+                paths.append(''.join(reversed(path)))
+            dfs(node.left, path)
+            dfs(node.right, path)
+            path.pop()
+
+        paths = []
+        dfs(root, [])
+        return min(paths)
