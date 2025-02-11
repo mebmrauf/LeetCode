ddcf27df12d7a81c6383afa648425666690eb15b
B M Rauf <bmrauf@outlook.com>
2025-02-12 00:54:15 +0600

    create

---
+++ Medium/1910. Remove All Occurrences of a Substring.py
@@ -0,0 +1,22 @@
+# 1910. Remove All Occurrences of a Substring
+# https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+
+# Using Stack
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        stack, leng = [], len(part)
+
+        for i in s:
+            stack.append(i)
+
+            if len(stack) >= leng and ''.join(stack[-leng:]) == part:
+                del stack[-leng:]
+
+        return ''.join(stack)
+
+# Using String Replace    
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        while part in s:
+            s = s.replace(part, "", 1)
+        return s
