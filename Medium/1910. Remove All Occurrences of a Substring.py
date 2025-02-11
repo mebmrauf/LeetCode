613d0343eaf12b4287a01866b1d8d0c876f4cb3e
B M Rauf <bmrauf@outlook.com>
2025-02-12 01:03:33 +0600

    update

--- Medium/1910. Remove All Occurrences of a Substring.py
+++ Medium/1910. Remove All Occurrences of a Substring.py
@@ -20,3 +20,13 @@ class Solution:
         while part in s:
             s = s.replace(part, "", 1)
         return s
+
+# Using String Find    
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        while True:
+            index = s.find(part)
+            if index == -1:
+                break
+            s = s[:index]+s[index+len(part):]
+        return s
